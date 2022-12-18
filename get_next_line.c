829e5499d51b40457ad3ffdab8280ddcc6678c55
SEÇKİN ERSİN <ozkan003@gmail.com>
2022-12-18 23:50:53 +0300

    v1.1

--- get_next_line.c
+++ get_next_line.c
@@ -6,91 +6,15 @@
 /*   By: seozkan <seozkan@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 13:56:37 by seozkan           #+#    #+#             */
-/*   Updated: 2022/12/18 13:56:37 by seozkan          ###   ########.fr       */
+/*   Updated: 2022/12/18 23:42:41 by seozkan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	if (!s)
-		return (0);
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	if (!s && c != '\0')
-		return (0);
-	while (*s)
-	{
-		if (*s == (unsigned char)c)
-			return ((char *)(s));
-		s++;
-	}
-	if (*s == (unsigned char)c)
-		return ((char *)(s));
-	return (0);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	len_src;
-
-	i = 0;
-	len_src = ft_strlen(src);
-	if (!src || !dst)
-		return (0);
-	if (dstsize == 0)
-		return (len_src);
-	while (src[i] && i < (dstsize - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (len_src);
-}
-
-char	*ft_strjoin(char *left_str, char *buff)
-{
-	size_t	i;
-	size_t	j;
-	char	*str;
-
-	if (!left_str)
-	{
-		left_str = (char *)malloc(1 * sizeof(char));
-		left_str[0] = '\0';
-	}
-	if (!left_str || !buff)
-		return (NULL);
-	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
-	if (str == NULL)
-		return (NULL);
-	i = -1;
-	j = 0;
-	if (left_str)
-		while (left_str[++i] != '\0')
-			str[i] = left_str[i];
-	while (buff[j] != '\0')
-		str[i++] = buff[j++];
-	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
-	free(left_str);
-	return (str);
-}
 
 char	*ft_get_line(char *left_str)
 {
-	int		i;
+	size_t		i;
 	char	*str;
 
 	i = 0;
@@ -109,26 +33,21 @@ char	*ft_get_line(char *left_str)
 
 char	*ft_new_left_str(char *left_str)
 {
-	int		i;
-	int		j;
+	size_t		n_size;
+	size_t		i;
 	char	*str;
-
-	i = 0;
-	while (left_str[i] && left_str[i] != '\n')
-		i++;
-	if (!left_str[i])
+	
+	if (!ft_strchr(left_str,'\n'))
 	{
 		free(left_str);
 		return (NULL);
 	}
-	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+	n_size = ft_strchr(left_str,'\n') - left_str;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - n_size));
 	if (!str)
 		return (NULL);
-	i++;
-	j = 0;
-	while (left_str[i])
-		str[j++] = left_str[i++];
-	str[j] = '\0';
+	i = ft_strlen(left_str) - n_size;
+	ft_strlcpy(str,left_str + n_size + 1, i);
 	free(left_str);
 	return (str);
 }
