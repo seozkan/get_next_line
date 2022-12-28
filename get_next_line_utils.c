decc9319733c87333ffc97ae94423b9966012afd
SEÇKİN ERSİN <ozkan003@gmail.com>
2022-12-28 14:42:59 +0300

    v1.9

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -57,38 +57,23 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	len_src;
-
-	i = 0;
-	len_src = ft_strlen(src);
-	if (!src || !dst)
-		return (0);
-	if (dstsize == 0)
-		return (len_src);
-	while (src[i] && i < (dstsize - 1))
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (len_src);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*join;
-	int		len_s1;
-	int		len_s2;
+	int		i;
+	int		j;
 
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	join = (char *)malloc(len_s1 + len_s2 + 1);
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	join = (char *)malloc(i + j + 1);
+	i = -1;
+	j = -1;
 	if (!s1 || !s2 || !join)
 		return (0);
-	ft_strlcpy(join, s1, len_s1 + 1);
-	ft_strlcpy(join + len_s1, s2, len_s2 + 1);
+	while (s1[++i])
+		join[i] = s1[i];
+	while (s2[++j])
+		join[i + j] = s2[j];
+	join[i + j] = '\0';
 	return (join);
 }
